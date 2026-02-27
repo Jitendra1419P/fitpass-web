5d94eb5a1e9bc6e2f8aeab1d0e7a8ef651735c6a
Jitendra1419P <jitendra1419@gmail.com>
2026-02-27 16:45:04 +0530

    Add queries, app components, constants, and classes page

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -1,8 +1,10 @@
+import { AppHeader } from "@/components/app/layout/AppHeader";
 import { SanityLive } from "@/sanity/lib/live";
 import { ClerkProvider } from "@clerk/nextjs";
 
 const AppLayout = ({ children }: { children: React.ReactNode }) => {
   return <ClerkProvider>
+    <AppHeader />
     {children}
     <SanityLive />
     </ClerkProvider>;
